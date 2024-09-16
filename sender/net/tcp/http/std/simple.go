db445eaa02f83c7713f14b28bb77904f4819f494
yanagitani <takanori.yanagitani@gmail.com>
2024-09-16 11:15:26 +0900

    feat: simple sender using net/http(std)

---
+++ sender/net/tcp/http/std/simple.go
@@ -0,0 +1,98 @@
+package hstd
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+
+	pm "github.com/takanoriyanagitani/go-post-many-http"
+)
+
+type SenderStdST struct {
+	*http.Client
+	StatusHandler func(pm.TinyResponse) error
+	ResponseConv  func(*http.Response) (pm.TinyResponse, error)
+}
+
+func (s SenderStdST) Post(
+	url string,
+	contentType string,
+	body []byte,
+) (*http.Response, error) {
+	return s.Client.Post(url, contentType, bytes.NewReader(body))
+}
+
+func (s SenderStdST) Handle(req pm.SimpleRequest) (pm.TinyResponse, error) {
+	var url string = req.Url
+	var typ string = req.ContentType
+	var bdy []byte = req.Body
+	res, e := s.Post(url, typ, bdy)
+	if nil != e {
+		return pm.TinyResponse{}, fmt.Errorf(
+			"unable to post(url=%s): %w",
+			url,
+			e,
+		)
+	}
+
+	return s.ResponseConv(res)
+}
+
+func (s SenderStdST) ToSender() pm.SenderST {
+	return func(
+		_ context.Context,
+		req pm.SimpleRequest,
+	) (pm.TinyResponse, error) {
+		return s.Handle(req)
+	}
+}
+
+func ResponseToTinyDiscard(
+	original *http.Response,
+) (pm.TinyResponse, error) {
+	var bdy io.ReadCloser = original.Body
+	defer bdy.Close()
+	_, e := io.Copy(io.Discard, bdy)
+	if nil != e {
+		return pm.TinyResponse{}, fmt.Errorf("unable to discard body: %w", e)
+	}
+	return pm.TinyResponse{StatusCode: original.StatusCode}, nil
+}
+
+func StatusHandlerNew(acceptStatus int) func(pm.TinyResponse) error {
+	return func(res pm.TinyResponse) error {
+		var sts int = res.StatusCode
+		switch sts {
+		case acceptStatus:
+			return nil
+		default:
+			return fmt.Errorf("unexpected http status: %v", sts)
+		}
+	}
+}
+
+var StatusHandlerDefault func(pm.TinyResponse) error = StatusHandlerNew(
+	http.StatusOK,
+)
+
+func SenderStdNewST(
+	client *http.Client,
+	statusHandler func(pm.TinyResponse) error,
+	responseConverter func(*http.Response) (pm.TinyResponse, error),
+) SenderStdST {
+	return SenderStdST{
+		Client:        client,
+		StatusHandler: statusHandler,
+		ResponseConv:  responseConverter,
+	}
+}
+
+func SenderStdNewSTdefault(client *http.Client) SenderStdST {
+	return SenderStdNewST(
+		client,
+		StatusHandlerDefault,
+		ResponseToTinyDiscard,
+	)
+}
