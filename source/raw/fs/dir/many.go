b0a40fc53187a6b68fa8ff0337252fbfab535827
yanagitani <takanori.yanagitani@gmail.com>
2024-09-16 11:15:11 +0900

    feat: raw request source using filesystem

---
+++ source/raw/fs/dir/many.go
@@ -0,0 +1,74 @@
+package rmany
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"io/fs"
+
+	pm "github.com/takanoriyanagitani/go-post-many-http"
+)
+
+type RawSourceManyFsDir struct {
+	// The trusted path to the directry.
+	TrustedDirName string
+
+	fs.ReadDirFS
+
+	MaxBodySize int64
+}
+
+func (r RawSourceManyFsDir) ReaderToBuffer(
+	rdr io.Reader,
+	buf *bytes.Buffer,
+) error {
+	buf.Reset()
+	limited := &io.LimitedReader{
+		R: rdr,
+		N: r.MaxBodySize,
+	}
+	_, e := io.Copy(buf, limited)
+	return e
+}
+
+func (r RawSourceManyFsDir) FilenameToBuffer(
+	filename string,
+	buf *bytes.Buffer,
+) error {
+	f, e := r.ReadDirFS.Open(filename)
+	if nil != e {
+		return e
+	}
+	defer f.Close()
+	return r.ReaderToBuffer(f, buf)
+}
+
+func (r RawSourceManyFsDir) ToRawRequestSourceMany(
+	eof error,
+) pm.RawRequestSourceMany {
+	dirents, e := r.ReadDirFS.ReadDir(r.TrustedDirName)
+	var buf bytes.Buffer
+	return func(ctx context.Context) (pm.RawRequest, error) {
+		if nil != e {
+			return pm.RawRequest{}, e
+		}
+
+		if nil != ctx.Err() {
+			return pm.RawRequest{}, ctx.Err()
+		}
+
+		if 0 == len(dirents) {
+			return pm.RawRequest{}, eof
+		}
+
+		var lix int = len(dirents) - 1
+		var last fs.DirEntry = dirents[lix]
+		var name string = last.Name()
+		err := r.FilenameToBuffer(name, &buf)
+		if nil != err {
+			return pm.RawRequest{}, err
+		}
+		dirents = dirents[:lix]
+		return buf.Bytes(), nil
+	}
+}
